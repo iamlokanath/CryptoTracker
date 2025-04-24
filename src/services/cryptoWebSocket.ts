dbbd0d8f20eb98195f09d101494fb454b2b58995
Lkanath Panda <lokanthpanda128@gmai.com>
2025-04-24 16:18:21 +0530

    searching issue fixed

--- src/services/cryptoWebSocket.ts
+++ src/services/cryptoWebSocket.ts
@@ -1,4 +1,4 @@
-import { setAssets, setStatus, setError, CryptoAsset } from '../features/crypto/cryptoSlice';
+import { setOriginalAssets, setStatus, setError, CryptoAsset } from '../features/crypto/cryptoSlice';
 // updatePrices,
 
 import { store } from '../app/store';
@@ -157,7 +157,7 @@ class CryptoWebSocket {
         });
         
         console.log(`Dispatching ${assets.length} assets`);
-        store.dispatch(setAssets(assets));
+        store.dispatch(setOriginalAssets(assets));
       }
     } catch (error) {
       console.error('Error processing WebSocket data:', error);
